ebcd93aa157dbd7078355d394e1e0188bd75cfc6
Baekjonghyeon <uoou9677@gmail.com>
2025-11-19 16:13:02 +0900

    2025-11-19

---
+++ web-study-11/src/main/java/com/saeyan/controller/ActionFactory.java
@@ -0,0 +1,29 @@
+package com.saeyan.controller;
+
+import com.saeyan.controller.action.Action;
+import com.saeyan.controller.action.BoardListAction;
+
+public class ActionFactory {
+	
+	private static ActionFactory instance  = new ActionFactory();
+	
+	private ActionFactory() {}
+	
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+	                           //board_insert
+	public Action getAction(String command) {
+		
+		Action action  = null;
+		
+		System.out.println("ActionFactory : " + command);
+		
+		//조건문
+		if(command.equals("board_list")) {
+			action =  new BoardListAction();
+		}
+		
+		return action;
+	}
+}
---
+++ web-study-11/src/main/java/util/DBManager.java
@@ -0,0 +1,66 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class DBManager {
+	
+	/* - mYsql 연결
+	 * 	1. 드라이브로드
+	 * 	2. url, id, password 이용해서 접속
+	 * 	3. 
+	*/
+	
+	public static Connection getConnection() {
+			
+			Connection con = null;
+			
+		try {
+			
+			//드라이브 로드
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			
+			// 연결 정보
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/edudb?serverTimezone=Asia/Seoul",
+					"jdbctest",
+					"1234"
+					); 
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}	// end Connection
+	
+	
+	// select 사용하고 자원 반납
+	public static void close(Connection con, Statement stmt, ResultSet rs) {
+		
+		try {
+			rs.close();
+			stmt.close();
+			con.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}// end close
+	
+	// update, delete, insert 사용하고 자원 반납
+	public static void close(Connection con, Statement stmt) {
+		
+		try {
+			stmt.close();
+			con.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}	//end DBM
